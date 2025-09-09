1cbbe605a9f9099aa6d12671fe4b714aef1568d1
csuarezgurruchaga <csuarezgurruchaga@gmail.com>
2025-09-09 10:49:46 -0300

    port fixed

--- main.py
+++ main.py
@@ -122,5 +122,5 @@ async def reset_conversation(numero_telefono: str = Form(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
